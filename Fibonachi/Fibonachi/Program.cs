17171fab5bc521d408743cbdca23989e7679b6a4
Anton Pylypchuk <skete2002@gmail.com>
2022-12-01 10:21:31 +0200

    fibonachi

---
+++ Fibonachi/Fibonachi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateFibonachi(5));
+            Console.WriteLine(CalculateFibonachi(6));
+            Console.WriteLine(CalculateFibonachi(1));
+            Console.WriteLine(CalculateFibonachi(10));
+        }
+
+        public static int CalculateFibonachi(int count)
+        {
+            if (count < 0)
+                return -1;
+            else if (count == 0)
+                return 0;
+            else if (count == 1)
+                return 1;
+
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+
+            for (int i = 0; i < count - 3; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return c;
+        }
+    }
+}
